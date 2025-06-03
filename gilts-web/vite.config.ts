830fdef0845a7ea7a79fabca211dc109628c88ae
benritz <23510442+benritz@users.noreply.github.com>
2025-06-03 08:03:24 +0100

    web ux - minify

--- gilts-web/vite.config.ts
+++ gilts-web/vite.config.ts
@@ -1,9 +1,24 @@
 import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    createHtmlPlugin({
+      minify: {
+        collapseWhitespace: true,
+        keepClosingSlash: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+        minifyCSS: true,
+        minifyJS: true,
+
+      },
+    })
   ],
   esbuild: {
     jsxFactory: 'h',
